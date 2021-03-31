1fbfebe9600d3fd8e0b0c12a60eb249e3feb6450
erellsworth <edward.ellsworth@gmail.com>
2021-03-31 15:00:18 -0400

    Expand markets api test

--- tests/markets.api.test.js
+++ tests/markets.api.test.js
@@ -11,12 +11,10 @@ describe('/api/markets', () => {
         await MarketsApiHandler(req, res);
 
         expect(res._getStatusCode()).toBe(200);
-        //   expect(JSON.parse(res._getData())).toEqual(
-        //     expect.objectContaining({
-        //       message: 'Your favorite animal is dog',
-        //     }),
-        //   );
 
-        done();
+        const data = JSON.parse(res._getData());
+
+        expect(Array.isArray(data.markets)).toBe(true);
+
     });
 });
